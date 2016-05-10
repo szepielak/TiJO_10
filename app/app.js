147310d0c759b472febaef4f75772e776af049d3
aniaw <ania_w@outolook.com>
2016-05-10 19:30:58 +0200

    add calculateArea function

--- app/app.js
+++ app/app.js
@@ -29,6 +29,25 @@
                 age--;
             }
             return age;
+        },
+        calculateArea: function (a, b, c, messageSuccess, messageError) {
+            var area;
+            if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number' ||
+                typeof messageSuccess !== 'string' || typeof messageError !== 'string') {
+                return false;
+            }
+            else {
+                area = a > b ? a - b * c : b - a * c;
+                if (area > 0) {
+                    return {area: area, message: messageSuccess};
+                }
+                else if (area < 0) {
+                    return {area: area, message: messageError};
+                }
+                else {
+                    return {area: 0, message: 'Big null'};
+                }
+            }
         }
     }
 })();
