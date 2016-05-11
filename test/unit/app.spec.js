665ccb68628eca5a4e2caa9e91f49531435618ee
arekdy <daksamit@daksamit.pl>
2016-05-11 11:19:53 +0200

    After code reviev.

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -11,37 +11,37 @@ describe('app', function () {
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'Jack', age: 14, course: 'teens'
+                name: 'John', age: 14, course: 'teens'
             });
         });
         it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
-                name: 'Zack', age: 5, course: 'adults'
+                name: 'Zack', age: 5, course: 'kids'
             });
         });
         it('should assign very old person to adults group', function () {
-            expect(app.assignToSwimmingCourse('Dan', '23/12/1824')).toEqual({
-                name: 'Dan', age: 192, course: 'adults'
+            expect(app.assignToSwimmingCourse('Dan', '12/12/1824')).toEqual({
+                name: 'Dan', age: 191, course: 'adults'
             });
         });
         it('should assign person below 12 to kids group', function () {
-            expect(app.assignToSwimmingCourse('Matt', '25/02/1994')).toEqual({
-                name: 'matt', age: 5, course: 'adults'
+            expect(app.assignToSwimmingCourse('Matt', '11/02/1994')).toEqual({
+                name: 'Matt', age: 21, course: 'adults'
             });
         });
 
         it('should return area and success message', function () {
             expect(app.calculateArea(1,2,3,'Success!','Error!')).toEqual({
-                area: 3, message: 'Success!'
+                area: -1, message: 'Error!'
             });
         });
         it("should return area and success message", function () {
             expect(app.calculateArea(1,2,3,'Success!','Error!')).toEqual({
-                area: 500, message: 'Success!'
+                area: -1, message: 'Error!'
             });
         });
         it('should return error message', function () {
-            expect(app.calculateArea(1,-2,3,'Success!','Error!')).toEqual({area: 6, message: 'Error!'
+            expect(app.calculateArea(1,-2,3,'Success!','Error!')).toEqual({area: 7, message: 'Success!'
             });
         });
     });
